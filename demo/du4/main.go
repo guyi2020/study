e8c3c84a57b865edd705e6a2cd91b3d424c44299
guyi2020 <you@example.com>
2020-12-06 22:10:40 +0800

    添加du的demo

--- demo/du4/main.go
+++ demo/du4/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -14,6 +13,9 @@ import (
 
 func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 	defer n.Done()
+	if cancelled() {
+		return
+	}
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			n.Add(1)
@@ -29,8 +31,19 @@ var sema = make(chan struct{}, 20)
 
 // 返回dir目录中的条目
 func dirents(dir string) []os.FileInfo {
-	sema <- struct{}{}
+	select {
+	case sema <- struct{}{}:
+	case <-done:
+		return nil
+	}
 	defer func() { <-sema }()
+
+	f, err := os.Open(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
+	}
+	defer f.Close()
 
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -40,17 +53,29 @@ func dirents(dir string) []os.FileInfo {
 	return entries
 }
 
-var verbose = flag.Bool("v", false, "show verbose progress message ")
+var done = make(chan struct{})
+
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
 
 func main() {
 	// 确定初始目录
 	// 解析参数
-	flag.Parse()
-
-	roots := flag.Args()
+	roots := os.Args[1:]
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
 
 	// 遍历文件树
 	fileSizes := make(chan int64)
@@ -67,13 +92,16 @@ func main() {
 
 	// 定期输出结果
 	var tick <-chan time.Time
-	if *verbose {
-		tick = time.Tick(500 * time.Millisecond)
-	}
 	var nfiles, nbytes int64
+
 loop:
 	for {
 		select {
+		case <-done:
+			for range fileSizes {
+
+			}
+			return
 		case size, ok := <-fileSizes:
 			if !ok {
 				break loop
